c7359e2876f764b9e7b3d6de2f974f54690c6be9
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-14 21:45:42 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,7 +10,14 @@ required_version=">=1.5.0"
 provider "azurerm"{
   features {}
   skip_provider_registration="true"
+
+subscription_id=var.subscription_id
+client_id=var.client_id
+clientsecret=var.client_secret
+tenant_id=var.tenent_id
 }
+
+
 
 
 
