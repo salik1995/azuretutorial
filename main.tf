26b0c2980c0492a23f0190c0f422e203d10d18e3
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-16 19:49:08 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "azurerm_storage_account" "awp" {
   name                     = "${var.prefix}storageaccountname${var.env}"
   resource_group_name      = azurerm_resource_group.tutorial.name
   location                 = azurerm_resource_group.tutorial.location
-  account_tier             = "Standard"
+  account_tier             = var.storage
   account_replication_type = "GRS"
 
   tags = {
