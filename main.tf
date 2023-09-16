f7d2dc07443e3dc11d645f3053e403d5ef8ce95b
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-16 19:18:34 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "tutorial" {
   name     = "${var.prefix}_resource_group"
   location = "Canada Central"
 }
+
+resource "azurerm_storage_account" "awp" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
