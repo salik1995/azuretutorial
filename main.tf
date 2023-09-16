3b9bc603ef21e6920f42efbbe36357f57a39ef89
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-16 19:40:49 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "tutorial" {
 }
 
 resource "azurerm_storage_account" "awp" {
-  name                     = "storageaccountname"
+  name                     = "${var.prefix}storageaccountname"
   resource_group_name      = azurerm_resource_group.tutorial.name
   location                 = azurerm_resource_group.tutorial.location
   account_tier             = "Standard"
