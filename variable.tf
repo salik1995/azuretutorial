a8d250d81bb9167eb81009b3fad73fdb25461767
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-16 18:43:06 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,15 @@
-variable="subscription_Id"
+variable "client_id" {
+  type=string
+}
+
+variable "client_secret" {
+  type=string
+}
+
+variable "subscription_id" {
+  type=string
+}
+
+variable "tenant_id" {
+  type=string
+}
