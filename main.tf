54650e73f148fc0bbe562f18738c4bb923c6365b
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-20 20:57:39 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,13 +14,13 @@ resource "azurerm_storage_account" "awp" {
     environment = "staging"
   }
 
-resource "azurerm_storage_container" "security" {
+resource "azurerm_storage_container" "security" "awp"{
   name                     = "${var.prefix}storage${var.env}"
   storage_account_name  = azurerm_storage_account.tutorial.name
   container_access_type = "private"
 }
 
-resource "azurerm_storage_blob" "tutoral" {
+resource "azurerm_storage_blob" "tutoral" "awp"{
   name                     = "${var.prefix}storage${var.env}"
   storage_account_name   = azurerm_storage_account.batch06.name
   storage_container_name = azurerm_storage_container.batch06.name
