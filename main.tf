34237ee52c66a05bc9fae11c631c118750c09c7a
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-21 20:15:53 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -39,4 +39,37 @@ resource "azurerm_sql_server" "trial" {
   tags = {
     environment = "production"
   }
+
+resource "azurerm_kubernetes_cluster" "firstclass" {
+  name                = "${var.prefix}storage${var.env}"
+  location            = azurerm_resource_group.tutorial.location
+  resource_group_name = azurerm_resource_group.tutorial.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "${var.prefix}storage${var.env}"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = azurerm_kubernetes_cluster.example.kube_config.0.client_certificate
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
+
  }
