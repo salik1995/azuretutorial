559abc3f9f625c63856cd8e0ae51966db4b7546b
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-21 20:37:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ output "client_certificate" {
 }
 
 output "kube_config" {
-  value = azurerm_kubernetes_cluster.example.kube_config_raw
+  value = azurerm_kubernetes_cluster.firstclass.kube_config_raw
 
   sensitive = true
 }
