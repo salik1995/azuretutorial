2fe15bca3bdc043cfa90414b681cdda228cdc4ed
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-21 21:27:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -173,14 +173,14 @@ resource "azurerm_virtual_network" "trainning" {
 resource "azurerm_subnet" "frontend" {
   name                 = "frontend"
   resource_group_name  = azurerm_resource_group.tutorial.name
-  virtual_network_name = azurerm_virtual_network.tutorial.trainning
+  virtual_network_name = azurerm_virtual_network.trainning.name
   address_prefixes     = ["10.254.0.0/24"]
 }
 
 resource "azurerm_subnet" "backend" {
   name                 = "backend"
   resource_group_name  = azurerm_resource_group.tutorial.name
-  virtual_network_name = azurerm_virtual_network.tutorial.trainning
+  virtual_network_name = azurerm_virtual_network.trainning.name
   address_prefixes     = ["10.254.2.0/24"]
 }
 
