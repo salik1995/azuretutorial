c8bc660020d2bfee29f4e2940f20f251092d6a65
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-28 10:49:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -295,7 +295,7 @@ resource "azurerm_container_group" "storage" {
   name                = "example-continst"
   location            = "${azurerm_resource_group.tutorial.location}"
   resource_group_name = "${azurerm_resource_group.tutorial.name}"
-  ip_address_type     = "public"
+  ip_address_type     = "private"
   dns_name_label      = "aci-label"
   os_type             = "Linux"
 
