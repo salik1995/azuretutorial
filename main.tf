7e28d3c9038af5b8e55b56708de40baf05c7ee71
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-28 13:18:58 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -349,20 +349,20 @@ resource "azurerm_kubernetes_cluster_node_pool" "joined" {
 }
 
 resource "azurerm_data_factory" "java" {
-  name                = "example"
+  name                = "${var.prefix}_resource_group_${var.env}"
   location            = "${azurerm_resource_group.tutorial.location}"
   resource_group_name = "${azurerm_resource_group.tutorial.name}"
 }
 
 resource "azurerm_data_factory_linked_service_mysql" "mysqlno" {
-  name                = "example"
+  name                = "${var.prefix}_resource_group_${var.env}"
   resource_group_name = "${azurerm_resource_group.example.name}"
   data_factory_name   = "${azurerm_data_factory.java.name}"
   connection_string   = "Server=test;Port=3306;Database=test;User=test;SSLMode=1;UseSystemTrustStore=0;Password=test"
 }
 
 resource "azurerm_data_factory_dataset_mysql" "azureyes" {
-  name                = "example"
+  name                = "${var.prefix}_resource_group_${var.env}"
   resource_group_name = "${azurerm_resource_group.tutorial.name}"
   data_factory_name   = "${azurerm_data_factory.java.name}"
   linked_service_name = "${azurerm_data_factory_linked_service_mysql.mysqlno.name}"
