e3df924b0f7449b85503efc828765709e6cac9b3
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-28 22:16:27 -0400

    Create kubernetes.tf

---
+++ kubernetes.tf
@@ -0,0 +1,36 @@
+locals {
+   cluster_names=["batch123","batch456","batch678","batch098","batch3232"]
+
+
+resource "azurerm_kubernetes_cluster" "mygroup" {
+  for_each            =  {for cluster in local.cluster_names: cluster=>cluster}
+  name                = "{$var.prefix}cluster-$(each.key)"
+  location            = azurerm_resource_group.tutorial.location
+  resource_group_name = azurerm_resource_group.tutorial.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+output "client_certificate" {
+  value     = [for cluster in azurerm_kubernetes_cluster.mygroup:cluster.kube_config.0.client_certificate]
+  sensitive = true
+}
+
+output "kube_config" {
+  value = azurerm_kubernetes_cluster.example.kube_config_raw
+
+  sensitive = true
+}
