63e9c0a61af9a0bf3c920ca1ec6061706c0df9ca
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-09-30 18:26:37 -0400

    Update virtual.tf

--- virtual.tf
+++ virtual.tf
@@ -1,22 +1,24 @@
-
+locals {
+   virtual_names=["azure","AWS","GCP","Mirco""Mircotech"]
+}
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.tutorial.location
+  resource_group_name = azurerm_resource_group.tutorial.name
 }
 
 resource "azurerm_subnet" "internal" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
+  resource_group_name  = azurerm_resource_group.tutorial.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.tutorial.location
+  resource_group_name = azurerm_resource_group.tutorial.name
 
   ip_configuration {
     name                          = "testconfiguration1"
@@ -26,8 +28,9 @@ resource "azurerm_network_interface" "main" {
 }
 
 resource "azurerm_virtual_machine" "main" {
+  for_each              =  {for virtual in local.virtual_names: virtual=>virtual}
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.tutorial.location
+  resource_group_name   = azurerm_resource_group.tutorial.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = "Standard_DS1_v2"
