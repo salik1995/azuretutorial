db0aa04fc7195eeb4fd1a15af2fa9c7d1562d33d
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-03 16:06:52 -0400

    Update windows.tf

--- windows.tf
+++ windows.tf
@@ -14,7 +14,7 @@ resource "azurerm_service_plan" "deploying" {
 resource "azurerm_windows_web_app" "start" {
   for_each            =  {for windows in local.windows_names: windows=>windows}
   name                = each.key
-  resource_group_name = azurerm_resource_group.example.name
+  resource_group_name = azurerm_resource_group.tutorial.name
   location            = azurerm_service_plan.deploying["w1"].location
   service_plan_id     = azurerm_service_plan.deploying["w1"].id
 
