20fe7cb1f79efe530942b63b340d01cb27e237a0
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-03 16:44:52 -0400

    Update windwepappslot.tf

--- windwepappslot.tf
+++ windwepappslot.tf
@@ -14,15 +14,15 @@ resource "azurerm_windows_web_app" "data" {
   for_each            =  {for wwindow in local.wwindow_names: wwindow=>wwindow}
   name                = "example-windows-web-app"
   resource_group_name = azurerm_resource_group.tutorial.name
-  location            = azurerm_service_plan.sim["wowabc"].location
-  service_plan_id     = azurerm_service_plan.sim["wowabc"].id
+  location            = azurerm_service_plan.sim["wowxabc"].location
+  service_plan_id     = azurerm_service_plan.sim["wowxabc"].id
 
   site_config {}
 }
 
 resource "azurerm_windows_web_app_slot" "animal" {
   name           = "example-slot"
-  app_service_id = azurerm_windows_web_app.data["wowabc"].id
+  app_service_id = azurerm_windows_web_app.data["wowxabc"].id
 
   site_config {}
 }
