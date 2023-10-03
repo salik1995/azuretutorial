f83198800f42213deb4c41151b01041110ff06cf
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-03 15:19:00 -0400

    Update linux.tf

--- linux.tf
+++ linux.tf
@@ -1,12 +1,16 @@
-resource "azurerm_service_plan" "example" {
+locals {
+   linux_names=["linuxabc","linuscdb","linuxbvc","linux098","linux3232"]
+}
+resource "azurerm_service_plan" "icecream" {
+  for_each            =  {for linux in local.linux_names: linux=>linux}
   name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.tutorial.name
+  location            = azurerm_resource_group.tutorial.location
   os_type             = "Linux"
   sku_name            = "P1v2"
 }
 
-resource "azurerm_linux_web_app" "example" {
+resource "azurerm_linux_web_app" "pops" {
   name                = "example"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
