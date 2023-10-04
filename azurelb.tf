0ad6847ae2fe4d872d5372373b4f76374bfedac1
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-03 22:38:30 -0400

    Update azurelb.tf

--- azurelb.tf
+++ azurelb.tf
@@ -32,3 +32,13 @@ resource "azurerm_lb_rule" "strong" {
   backend_port                   = 3389
   frontend_ip_configuration_name = "PublicIPAddress"
 }
+resource "azurerm_lb_nat_pool" "strongest" {
+  resource_group_name            = azurerm_resource_group.tutorial.name
+  loadbalancer_id                = azurerm_lb.designed.id
+  name                           = "SampleApplicationPool"
+  protocol                       = "Tcp"
+  frontend_port_start            = 80
+  frontend_port_end              = 81
+  backend_port                   = 8080
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
