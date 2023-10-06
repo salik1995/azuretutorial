683896814046891fb5b6a8c5f152ceedb8296e17
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-05 20:21:33 -0400

    Update mainn.tf

--- mainn.tf
+++ mainn.tf
@@ -16,8 +16,8 @@ locals{
 resource "azurerm_service_plan" "batcha06sp" {
   for_each            ={for sp in local.linux_app_list: "${sp.name}"=>sp }
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.tutorial.name
+  location            = azurerm_resource_group.tutorial.location
   os_type             = each.value.os_type
   sku_name            = each.value.sku_name
 }
@@ -25,8 +25,8 @@ resource "azurerm_service_plan" "batcha06sp" {
 resource "azurerm_linux_web_app" "batcha06webapp" {
   for_each            = azurerm_service_plan.batcha06sp
   name                = each.value.name
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.tutorial.name
+  location            = azurerm_resource_group.tutorial.location
   service_plan_id     = each.value.id
 
   site_config {}
