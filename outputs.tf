bdd5d1257c462879d9d0230fbbbc7249593b4391
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-05 21:28:39 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,15 @@
 output "student_names" {
   value = [for student in local.student_names: student]
 }
-output "student_names" {
-  value = [for student in local.student_names: student]
-}
+output "listofwindowsapp:
+  - name: firstwindowsapp    
+    os_type: window
+    sku_name: P1v2
+  - name: secondwindowsapp    
+    os_type: window
+    sku_name: P1v2
+  - name: thirdwindowsapp    
+    os_type: window
+    sku_name: P1v2" {
+  }
 
