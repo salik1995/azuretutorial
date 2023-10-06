bb9c51a46d1c8ba9b549478f2ea5c79598363544
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-05 20:41:11 -0400

    Update mainn.tf

--- mainn.tf
+++ mainn.tf
@@ -43,7 +43,7 @@ resource "azurerm_service_plan" "casting" {
 
 resource "azurerm_windows_web_app" "diet" {
   for_each            = azurerm_service_plan.casting
-  name                = "{$var.prefix}diet-$(each.key)"
+  name                = each.value.name
   resource_group_name = azurerm_resource_group.tutorial.name
   location            = azurerm_service_plan.casting[each.key].location
   service_plan_id     = azurerm_service_plan.casting[each.key].id
