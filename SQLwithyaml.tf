65e0167f585080c78ce09fd82d900488a1a19254
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-10 20:41:34 -0400

    Update SQLwithyaml.tf

--- SQLwithyaml.tf
+++ SQLwithyaml.tf
@@ -1,11 +1,11 @@
 locals{
   sql_server=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
   sql_server_list = system([
-    for app in local.sql_server : [
-      for linuxapps in try(app.listofsqlserver, []) :{
-        name=sqlserver.name
-        os_type=sqlserver.os_type
-        sku_name=sqlserver.sku_name 
+  for app in local.sql_server : [
+    for linuxapps in try(app.listofsqlserver, []) :{
+      name=sqlserver.name
+      os_type=sqlserver.os_type
+      sku_name=sqlserver.sku_name 
       }
     ]
 ])
