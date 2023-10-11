4cfa44dede25cb01639ed778f54bcf34e4d5fa00
salik1995 <129122912+salik1995@users.noreply.github.com>
2023-10-10 21:39:55 -0400

    Update SQLwithyaml.tf

--- SQLwithyaml.tf
+++ SQLwithyaml.tf
@@ -16,8 +16,8 @@ resource "azurerm_sql_server" "networking" {
   resource_group_name          = azurerm_resource_group.tutorial.name
   location                     = azurerm_resource_group.tutorial.location
   version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+  administrator_login          = "${var.administrator_login}"
+  administrator_login_password = "${var.administrator_login_password}"
 
   tags = {
     environment = "production"
